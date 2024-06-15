0f10beed0f3c6b67e04494b44a56cc5da038ef9f
sdunlap1 <sdunlap1@mac.com>
2024-06-14 21:30:26 -0700

    Made the image_url 255 chars

--- models.py
+++ models.py
@@ -10,7 +10,7 @@ class User(db.Model):
   id = db.Column(db.Integer, primary_key=True, autoincrement=True)
   first_name = db.Column(db.String(50), nullable=False)
   last_name = db.Column(db.String(50), nullable=False)
-  image_url = db.Column(db.String(50), nullable=False, default='https://via.placeholder.com/150')
+  image_url = db.Column(db.String(255), nullable=False, default='https://via.placeholder.com/150')
 
   def __repr__(self):
     return f'<User {self.first_name} {self.last_name}>'
